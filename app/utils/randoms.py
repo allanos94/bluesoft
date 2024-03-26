6b59eaa17ba65990406d1bf7ec71781bfc286c7e
Alberto Llanos <alberto.llanosco@gmail.com>
2024-03-26 13:06:46 -0500

    feat: add generate_uuid function to randoms.py

--- app/utils/randoms.py
+++ app/utils/randoms.py
@@ -1,10 +1,12 @@
-
-
-
 import random
 import string
+import uuid
 
 
 def generate_account_number():
     """ Generate a random account number """
     return ''.join(random.choices(string.digits, k=20))
+
+def generate_uuid():
+    """ Generate a random UUID """
+    return str(uuid.uuid4())
